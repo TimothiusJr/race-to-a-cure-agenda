1f77715d1ab9020e24eec2a08125fe650fa92043
Tee-jay Salmon <teejaysalmon7@gmail.com>
2026-07-16 11:42:56 -0400

    Crop header banner

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,28 +1,10 @@
 import "./Header.css";
+import banner from "../../assets/images/header/header-banner.png";
 
 export default function Header() {
     return (
-        <header className="event-header">
-            <div className="event-header__logo">
-                <div className="event-header__the">THE</div>
-
-                <div className="event-header__amazing">AMAZING</div>
-
-                <div className="event-header__race-row">
-                    <div className="event-header__race">RACE</div>
-                    <div className="event-header__blood-drop" aria-hidden="true" />
-                </div>
-
-                <div className="event-header__brush">
-                    <span>RACE TO A </span>
-                    <span className="event-header__cure">CURE</span>
-                </div>
-
-                <div className="event-header__hematology">
-                    <span>IN </span>
-                    <span>HEMATOLOGY</span>
-                </div>
-            </div>
-        </header>
+        <div className="header-banner">
+            <img src={banner} alt="Race to a Cure" />
+        </div>
     );
 }
